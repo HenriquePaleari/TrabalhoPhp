5bb5f03d0aa6c21a1a497109d6f9255e9d8779da
GabrielSheldes <gabriel.arantes(@etec.sp.gov.br>
2025-05-19 21:39:34 -0300

    Banco de Dados

--- BancoDeDados.php
+++ BancoDeDados.php
@@ -1,9 +1,31 @@
 <?php
 
 $dsn = "MySql:host=localhost;dbname=ProjetoPHP";
-$username = "Nome do usuario que esta no banco de dos";
-$password = "senha do usuario";
-$pdo = new PDO($dsn,$username,$password);
+$user = "Gabriel";
+$password = 'root';
+//$pdo = new PDO($dsn,$username,$password);
+
+try { $conexao = new PDO(dsn: $dsn, Username: $user,
+    password: $password);
+
+}
+catch(Exception $e){
+    echo "Erro de conexão";
+    exit;
+}
+
+$usuario = $_POST["usuario"];
+$senha = $_POST ["Senha"];
+
+
+$RESULTADO = $conexao -> query(query: "SELECT * FROM usuario where usuario = '$usuario' and senha = '$senha'");
+
+if($RESULTADO -> RowCount() > 0){
+    header(header: "location: inicial.php");
+} else {
+    header(header : "location: index.php?erro=1");
+}
+?>
 
 
 
