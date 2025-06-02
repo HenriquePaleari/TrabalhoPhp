88c03d83dd55300c742c9c89d0a7f3362e36cf20
GabrielSheldes <gabriel.arantes9@etec.sp.gov.br>
2025-06-02 20:11:40 -0300

    BancoDeDados

--- BancoDeDados.php
+++ BancoDeDados.php
@@ -3,14 +3,14 @@
 $dsn = "MySql:host=localhost;dbname=ControlePonto";
 $username = "usuario1";
 $password = 'root';
-//$pdo = new PDO($dsn,$username,$password);
+$pdo = new PDO($dsn,$username,$password);
 
-//$stmt = $pdo->query("SELECT * FROM ");
-//while ($usuario = $stmt->fetch(PDO::FECTCH_ASSOC)){
-//    echo $usuario ['id'] . "-" . $usuario['login'] . "-" . $usuario"senha" . "<br>";
-//}
+$stmt = $pdo->query("SELECT * FROM Usuarios");
+while ($usuario = $stmt->fetch(PDO::FECTCH_ASSOC)){
+   echo $usuario ['id'] . "-" . $usuario['login'] . "-" . $usuario ['Senha'] . "<br>";
+}
 
-try { $conexao = new PDO(dsn: $dsn, Username: $user,
+try { $conexao = new PDO($dsn, Username: $user,
      password: $password);
     }
 catch(Exception $erro){
@@ -25,9 +25,9 @@ $senha = $_POST ["Senha"];
 $RESULTADO = $conexao -> query(query: "SELECT * FROM usuario where usuario = '$usuario' and senha = '$senha'");
 
 if($RESULTADO -> RowCount() > 0){
-    header(header: "location: inicial.php");
+    header("location: inicial.php");
 } else {
-    header(header : "location: index.php?erro=1");
+    header( "location: index.php?erro=1");
 }
 ?>
 
